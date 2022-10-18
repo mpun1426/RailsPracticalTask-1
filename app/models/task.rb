dd823a357030ef244775336049d672846c7a2c7b
mpun1426 <mpun1426@gmail.com>
2022-10-19 04:03:14 +0900

    validation追加・変更、view微細変更、l18n国際化

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,20 @@
 class Task < ApplicationRecord
-  validates :title, length: {minimum: 1}
+  def date_check
+    if self.start_date.present? && self.end_date.present?
+      errors.add(:end_date, "は開始日以降の日付を選択してください") if self.end_date < self.start_date
+    end
+  end
+
+  def blank_title
+    unless self.title.present?
+      errors.add(:title, "は入力必須です")
+    end
+  end
+  
+  validate :blank_title
   validates :title, length: {maximum: 20}
-  validates :start_date, presence: true 
+  validates :start_date, presence: true
   validates :end_date, presence: true
+  validate :date_check
   validates :memo, length: {maximum: 147}
 end
